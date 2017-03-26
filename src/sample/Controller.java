530d0cb2c2044151d6424c117eeb63cb70b73481
Emil <nallieh1001@gmail.com>
2017-03-26 19:41:35 +0200

    syso2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    public void printMessage() {
+
+    }
+
+    public void printTwoMessages() {
+        System.out.println("test");
+        System.out.println("test");
+    }
 }
